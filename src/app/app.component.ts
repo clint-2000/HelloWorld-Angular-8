670a4c81ff5937dbfb4baca785a6eba1f856943d
trainning <trainning@trainning>
2022-10-12 14:14:33 +0530

    styled file updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+declare var anime: any;  // declare anime js here
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'helloApp';
+
+  ngAfterViewInit(): void {
+  
+    var container = {opacityIn: [],scaleIn: [],scaleOut: 0,durationIn: 0,durationOut: 0,delay: 0};
+    container.opacityIn = [0,1];
+    container.scaleIn = [0.2, 1];
+    container.scaleOut = 3;
+    container.durationIn = 800;
+    container.durationOut = 600;
+    container.delay = 500;
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.container .letters-1',
+    opacity: container.opacityIn,
+    scale: container.scaleIn,
+    duration: container.durationIn
+  }).add({
+    targets: '.container .letters-1',
+    opacity: 0,
+    scale: container.scaleOut,
+    duration: container.durationOut,
+    easing: "easeInExpo",
+    delay: container.delay
+  }).add({
+    targets: '.container .letters-2',
+    opacity: container.opacityIn,
+    scale: container.scaleIn,
+    duration: container.durationIn
+  }).add({
+    targets: '.container .letters-2',
+    opacity: 0,
+    scale: container.scaleOut,
+    duration: container.durationOut,
+    easing: "easeInExpo",
+    delay: container.delay
+  }).add({
+    targets: '.container',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
+}
 }
